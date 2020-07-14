89a1ac95084884e4eddb052bb6ee751aef45ecb0
bobkatla <bertram.99.works@gmail.com>
2020-07-14 16:21:17 +1000

    ADD: particles style of snow

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
 import React from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 
+// const pacOptions = {
+//   "particles": {
+//       "number": {
+//           "value": 50
+//       },
+//       "size": {
+//           "value": 3
+//       }
+//   },
+//   "interactivity": {
+//       "events": {
+//           "onhover": {
+//               "enable": true,
+//               "mode": "repulse"
+//           }
+//       }
+//   }
+// }
+
+const pacOptions = {
+  "particles": {
+      "number": {
+          "value": 160,
+          "density": {
+              "enable": false
+          }
+      },
+      "size": {
+          "value": 10,
+          "random": true
+      },
+      "move": {
+          "direction": "bottom",
+          "out_mode": "out"
+      },
+      "line_linked": {
+          "enable": false
+      }
+  },
+  "interactivity": {
+      "events": {
+          "onclick": {
+              "enable": true,
+              "mode": "remove"
+          }
+      },
+      "modes": {
+          "remove": {
+              "particles_nb": 10
+          }
+      }
+  }
+}
+
 function App() {
   return (
     <div className='App'>
+      <Particles className='particles' params={pacOptions} />
       <Navigation />
       <Logo />
       <Rank />
